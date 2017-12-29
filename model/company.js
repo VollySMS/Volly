78a93cf00a5c4edbbbde455612102615694382dd
Robert Reed <robert.mc.reed@gmail.com>
2017-12-29 15:01:28 -0800

    company model and methods written

---
+++ model/company.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const httpErrors = require('http-errors');
+const jsonWebToken = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const companySchema = mongoose.Schema({
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tokenSeed: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+companySchema.methods.verifyPassword = function(password) {
+  return bcrypt.compare(password, this.passwordHash)
+    .then(response => {
+      if(!response) {
+        throw new httpErrors(401, '__AUTH__ incorrect company name or password');
+      }
+      return this;
+    });
+};
+
+companySchema.methods.createToken = function() {
+  this.tokenSeed = crypto.randomBytes(64).toString('hex');
+  return this.save()
+    .then(company => {
+      return jsonWebToken.sign({
+        tokenSeed: company.tokenSeed,
+      }, process.env.SALT_SECRET);
+    });
+};
+
+const Company = module.exports = mongoose.model('companie', companySchema);
+
+Company.create = (companyName, password, email) => {
+  const HASH_SALT_ROUNDS = 8;
+  return bcrypt.hash(password, HASH_SALT_ROUNDS)
+    .then(passwordHash => {
+      let tokenSeed = crypto.randomBytes(64).toString('hex');
+      return new Company({
+        companyName,
+        passwordHash,
+        email,
+        tokenSeed,
+      }).save();
+    });
+};
+
