c24ee1e16db20d19ac56628ae14b0e9a18a7d6bc
Robert Reed <robert.mc.reed@gmail.com>
2017-12-29 16:20:26 -0800

    removed console logs and fixed server start log

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ require('dotenv').config();
 const server = require('./lib/server');
 
 server.start()
+  .then(() => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  })
   .catch(console.log);
