7e578227791bc473c7e4e2679307ee7b57ca8bfd
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-31 16:07:10 -0800

    company move volunteer from pending to active

--- route/company-auth-router.js
+++ route/company-auth-router.js
@@ -4,6 +4,7 @@ const {Router} = require('express');
 const jsonParser = require('express').json();
 const httpErrors = require('http-errors');
 const Company = require('../model/company');
+const Volunteer = require('../model/volunteer');
 const basicAuthCompany = require('../lib/basic-auth-middleware')(Company);
 const bearerAuthCompany = require('../lib/bearer-auth-middleware')(Company);
 
@@ -21,9 +22,8 @@ companyAuthRouter.post('/company/signup', jsonParser, (request, response, next) 
 });
 
 companyAuthRouter.get('/company/login', basicAuthCompany, (request, response, next) => {
-  if(!request.company) {
+  if(!request.company) 
     return next(new httpErrors(404, '__ERROR__ company not found'));
-  }
 
   return request.company.createToken()
     .then(token => response.json({token}))
@@ -31,9 +31,30 @@ companyAuthRouter.get('/company/login', basicAuthCompany, (request, response, ne
 });
 
 companyAuthRouter.get('/company/pending', bearerAuthCompany, (request, response, next) => {
-  if(!request.company) {
+  if(!request.company) 
     return next(new httpErrors(404, '__ERROR__ company not found'));
-  }
 
   return response.json({pendingVolunteers: request.company.pendingVolunteers});
+});
+
+companyAuthRouter.put('/company/approve', bearerAuthCompany, jsonParser, (request, response, next) => {
+  if(!request.company) 
+    return next(new httpErrors(404, '__ERROR__ company not found'));
+
+  return Volunteer.findById(request.body.volunteerId)
+    .then(volunteer => {
+      if(!volunteer) 
+        throw new httpErrors(404, '__ERROR__ volunteer not found');
+      
+      volunteer.activeCompanies.push(request.company._id);
+      volunteer.pendingCompanies = volunteer.pendingCompanies.filter(companyId => companyId.toString() !== request.company._id.toString());
+      return volunteer.save();        
+    })
+    .then(volunteer => {
+      request.company.activeVolunteers.push(volunteer._id);
+      request.company.pendingVolunteers = request.company.pendingVolunteers.filter(volunteerId => volunteerId.toString() !== volunteer._id.toString());
+      return request.company.save();
+    })
+    .then(company => response.json({pendingVolunteers: company.pendingVolunteers, activeVolunteers: company.activeVolunteers}))
+    .catch(next);
 });
